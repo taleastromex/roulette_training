b0685daa873df39ab89bb5714bd007239246f282
Davut <sergeev.davutt@gmail.com>
2026-07-06 20:36:39 +0400

    Added basic web-interface

---
+++ src/Application/Actions/IndexAction.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Application\Actions;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Log\LoggerInterface;
+use Slim\Views\Twig;
+
+class IndexAction extends Action
+{
+    public function __construct(LoggerInterface $logger, private Twig $twig)
+    {
+        parent::__construct($logger);
+    }
+
+    protected function action(): Response
+    {
+        return $this->twig->render(
+            $this->response,
+            'index.html.twig',
+            [
+                'testing_list' => [
+                    [
+                        'name' => 'Multiplications table testing',
+                        'slug' => 'multiplications-table',
+                    ]
+                ]
+            ]
+        );
+    }
+}
