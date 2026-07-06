462dfedec507b7b4b89bdbeeb922548ce85e0c2a
Davut <44943269+taleastromex@users.noreply.github.com>
2026-07-06 20:04:39 +0400

    Added basic infrastructure (#1)
    * Added basic infrastructure
    
    * fixed from ci
    
    * fixed from ci
    
    * removed php8.1 dependency
    
    * switched for php8.4
    
    * fixed error from ci
    
    * fixed error from ci
    
    * fixed error from ci

--- src/Infrastructure/Persistence/User/InMemoryUserRepository.php
+++ src/Infrastructure/Persistence/User/InMemoryUserRepository.php
@@ -16,17 +16,11 @@ class InMemoryUserRepository implements UserRepository
     private array $users;
 
     /**
-     * @param User[]|null $users
+     * @param User[] $users
      */
-    public function __construct(array $users = null)
+    public function __construct(array $users = [])
     {
-        $this->users = $users ?? [
-            1 => new User(1, 'bill.gates', 'Bill', 'Gates'),
-            2 => new User(2, 'steve.jobs', 'Steve', 'Jobs'),
-            3 => new User(3, 'mark.zuckerberg', 'Mark', 'Zuckerberg'),
-            4 => new User(4, 'evan.spiegel', 'Evan', 'Spiegel'),
-            5 => new User(5, 'jack.dorsey', 'Jack', 'Dorsey'),
-        ];
+        $this->users = $users;
     }
 
     /**
